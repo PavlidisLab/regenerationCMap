17a5b193e06da35946c9ca0d80a00780f70dd9c1
OganM <ogan.mancarcii@gmail.com>
2017-05-14 17:01:44 -0700

    connectivityMapEnrichment function

--- R/stats.R
+++ R/stats.R
@@ -51,3 +51,53 @@ scoreCalc = function(Vup,Vdown,n){
     score = score(kUp,kDown)
     return(data.frame(kUp = kUp, kDown = kDown,score = score,instance= colnames(Vup)))
 }
+
+#' @export
+connectivityMapEnrichment = function(upTags,downTags,rankMatrix,instances,d=100000){
+    n = rankMatrix %>% nrow 
+    Vup = rankMatrix[upTags,] %>% apply(2,sort)
+    Vdown = rankMatrix[downTags,] %>% apply(2,sort) 
+    scores = scoreCalc(Vup,Vdown,n)
+    
+    # "p to be max( si) and q to be min( si) across all instances in the collection c"
+    p = max(scores$score)
+    q = min(scores$score)
+    
+    scores %<>% dplyr::mutate(ConScore = (score>0)*(score/p) + (score<0)*(-score/q))
+    
+    # "The Kolmogorov-Smirnov statistic is computed for the set of t instances
+    # in the list of all n instances in a result ordered in descending order of
+    # connectivity score and up score (see how connectivity score is
+    # calculated), giving an enrichment score ks0."
+    scores = scores %>% dplyr::arrange(desc(ConScore),desc(kUp))
+    
+    chems = instances$cmap_name %>% unique
+    
+    
+    confidence = chems %>% sapply(function(chem){
+        print(chem)
+        chemInstances = rownames(instances)[instances$cmap_name %in% chem]
+        
+        relevantInstances = scores %>% dplyr::filter(instance %in% chemInstances)
+        
+        relevantUpCount = (relevantInstances$score>0) %>% sum
+        relevantDownCount = (relevantInstances$score<0) %>% sum
+        relevantMehCount = (relevantInstances$score==0) %>% sum
+        nonNull = (relevantUpCount>relevantDownCount)*relevantUpCount + 
+            (relevantUpCount<relevantDownCount)*(relevantDownCount)
+        nonNull = nonNull/nrow(relevantInstances)
+        
+        V = match(chemInstances,scores$instance) %>% sort
+        ks0 = ksCalc(V,nrow(instances))
+        
+        Vrandoms = memoRandomV(length(chemInstances),d)
+        ksPerm = memoKsCalc(Vrandoms,nrow(instances))
+        
+        q = sum(abs(ksPerm) >= abs(ks0))
+        p = q/d
+        
+        return(c(enrichment = ks0, p = p,nonNull = nonNull,instanceCount =  length(chemInstances)))
+    }) %>% t
+    
+    return(list(instanceScores = scores, chemScores = confidence))
+}
