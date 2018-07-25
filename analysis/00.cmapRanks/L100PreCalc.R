05cb7fa8ad0ce5216811298df01e4e34b966dc10
OganM <ogan.mancarcii@gmail.com>
2018-07-25 00:15:54 -0700

    not a sound from the pavement

---
+++ analysis/00.cmapRanks/L100PreCalc.R
@@ -0,0 +1,4 @@
+library(cmapQuery)
+instances = readRDS('analysis/00.cmapRanks/instances.rds')
+
+L1000PreCalc = preCalcRandomKs(instances$pert_iname,nro)
--- analysis/00.cmapRanks/cmapRanks.R
+++ analysis/00.cmapRanks/cmapRanks.R
@@ -1,6 +1,8 @@
 library(rhdf5)
 library(data.table)
 library(cmapR)
+library(magrittr)
+library(dplyr)
 cmapData = cmapR::parse.gctx('/space/scratch/nlim/LINCS/GSE92742-Level5/Level5_Data.gctx')
 print('read DE data')
 gc()
@@ -9,12 +11,12 @@ inst = readr::read_tsv('/space/scratch/nlim/LINCS/GSE92742-Level5/Sig_Info.txt')
 L1000geneAnnots = readr::read_tsv('/space/scratch/nlim/LINCS/GSE92742-Level5/Gene_Info.txt')
 print('read metadata')
 
+instanceOrder = colnames(cmapData@mat)
+geneOrder = rownames(cmapData@mat)
+
+L1000geneAnnots = L1000geneAnnots[match(geneOrder,L1000geneAnnots$pr_gene_id),]
+inst = inst[match(instanceOrder, inst$sig_id),]
 # measured or nicely imputed
-
-
-inst %<>% rename(cmap_name = pert_iname)
-rownames(inst) = inst$sig_id
-
 print('fiddling')
 
 
@@ -24,15 +26,31 @@ deMatrix = cmapData@mat[goodGenes,]
 
 print('good Genes filtered')
 
+goodChem = inst$pert_iname %>% table %>% {.>1} %>% which %>% names
+goodInstances = inst$pert_iname %in% goodChem
 
-rankMatrix = deMatrix %>% apply(2,frank)
+inst = inst[goodInstances,]
+deMatrix = deMatrix[,goodInstances]
+
+print('Removed unreliable experiments')
+
+
+rankMatrix = deMatrix %>% apply(2,frankv,order = -1)
 
 print('ranks calcualted')
 
-saveRDS(rankMatrix,file = 'analysis/00.cmapRanks/rankMatrix.rds')
-saveRDS(L1000geneAnnots,file = 'analysis/00.cmapRanks/rankMatrix.rds')
+saveRDS(rankMatrix,file = 'analysis/00.cmapRanks/rankMatrix.rds',compress= 'bzip2')
+saveRDS(L1000geneAnnots,file = 'analysis/00.cmapRanks/L1000geneAnnots.rds')
 saveRDS(inst,file = 'analysis/00.cmapRanks/instances.rds')
 
 print('data saved')
 
 # data.table::frankv
+
+
+
+
+# nathaniel's version
+"/home/omancarci/allDirs/home/nlim/MDE/RScripts/DataFreeze/Packaged/LINCS/GSE92742/fc.matrix.RDS.XZ"
+hede = readRDS("/home/nlim/MDE/RScripts/DataFreeze/Packaged/LINCS/GSE92742/Interim/final.assign.mapping.RDS.XZ")
+hodo = readRDS("/home/nlim/MDE/RScripts/DataFreeze/Packaged/LINCS/GSE92742/Interim/sample.mapping.RDS.XZ")
